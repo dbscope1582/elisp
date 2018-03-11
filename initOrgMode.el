1dcda6cc23137835a24fe89a12036b2a4a63a997
papa <papa@oistrach.fritz.box>
2018-03-11 17:37:08 +0100

    bring captures to work on oistrach

--- initOrgMode.el
+++ initOrgMode.el
@@ -4,16 +4,16 @@
   (let ()
     (message "db-init-org-mode start")
     (setq
-;;     db-org-work-dir "~/Documents/org/"
+     db-org-work-dir "~/Documents/org/"
      org-enforce-todo-dependencies t
      ;; does it work?
      org-agenda-dim-blocked-tasks t
      org-log-done 'note
      org-log-into-drawer t     
      org-archive-location "archive/%s::2018"
-;;     org-default-notes-file (concat db-org-work-dir "generalNotes.org")
+     org-default-notes-file (concat db-org-work-dir "generalNotes.org")
      )
-;;    (define-key global-map "\C-cc" 'org-capture)
+    (define-key global-map "\C-cc" 'org-capture)
     (global-set-key "\C-ca" 'org-agenda)
 ;;    (db-init-load-machine-file-if-exists "initOrgMode.el")
     (message "db-init-org-mode done")))
