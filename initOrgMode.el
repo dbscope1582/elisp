a328f7d642ce56af0de45349b5292d85f0e979b5
papa <papa@maxim.fritz.box>
2018-03-16 17:50:59 +0100

    add dropbox/mobileOrg support and add a local config file for maxim

--- initOrgMode.el
+++ initOrgMode.el
@@ -5,6 +5,7 @@
     (message "db-init-org-mode start")
     (setq
      db-org-work-dir "~/Documents/org/"
+     org-directory db-org-work-dir
      org-enforce-todo-dependencies t
      ;; does it work?
      org-agenda-dim-blocked-tasks t
@@ -12,6 +13,10 @@
      org-log-into-drawer t     
      org-archive-location "archive/%s::2018"
      org-default-notes-file (concat db-org-work-dir "generalNotes.org")
+     ;; the physical directory created by dropbox for its syncing
+     org-mobile-directory "~/Dropbox/MobileOrg"
+     ;; just an arbitrary file name: org-mode creates it and uses it
+     org-mobile-inbox-for-pull "~/OrgMobileInboxForPull"
      )
     (define-key global-map "\C-cc" 'org-capture)
     (global-set-key "\C-ca" 'org-agenda)
---
+++ machine/maxim_initOrgMode.el
@@ -0,0 +1,5 @@
+;; initialisation specific for maxim
+
+
+(load-file (concat (file-name-directory load-file-name) "scope_initOrgMode.el"))
+
