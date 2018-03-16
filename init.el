6d2fa674f34ce5166cfe673588f782e018560388
papa <papa@maxim.fritz.box>
2018-03-16 18:55:05 +0100

    add package repository

--- init.el
+++ init.el
@@ -31,6 +31,13 @@
     ;; add the recentfile list to the file menu
     (recentf-mode)
     (show-paren-mode)
+    (when (>= emacs-major-version 24)
+      (require 'package)
+      (add-to-list
+       'package-archives
+       ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+       '("melpa" . "http://melpa.milkbox.net/packages/")
+       t))
     (message "db-init-emacs done")
     ))
 
