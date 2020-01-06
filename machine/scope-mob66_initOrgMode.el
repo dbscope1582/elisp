56b14d973516a80da207d1ec6d3a60d16e46b58f
papa <dbscope1582@gmail.com>
2020-01-06 07:59:48 +0100

    add config file for the new laptop

---
+++ machine/scope-mob66_initOrgMode.el
@@ -0,0 +1,8 @@
+;; initialisation specific for scope-mob39
+
+
+(load-file (concat (file-name-directory load-file-name) "scope_initOrgMode.el"))
+
+;;(setq org-link-abbrev-alist
+;;	  '(("axosoftDefect"  . "http://vserver31/Axosoft/viewitem?id=%s&type=defects&force_use_number=true")
+;;	    ("axosoftStory" . "http://vserver31/Axosoft/viewitem?id=%s&type=features&force_use_number=true")))
