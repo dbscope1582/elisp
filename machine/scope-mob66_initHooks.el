2020ee7fe331793f5641897591c0ef56d64611c6
papa <dbscope1582@gmail.com>
2020-09-18 09:33:31 +0200

    map c# to c-mode, add already existing mapping (formerly in .emacs) of asciidoc to adoc-mode

--- machine/scope-mob66_initHooks.el
+++ machine/scope-mob66_initHooks.el
@@ -5,4 +5,7 @@
    '(lambda ()
       (display-line-numbers-mode)
       (auto-fill-mode -42 )
-      )))
+      ))
+  (add-to-list 'auto-mode-alist '("\\.asciidoc\\'" . adoc-mode))
+  (add-to-list 'auto-mode-alist '("\\.cs\\'" . c-mode))
+  )
