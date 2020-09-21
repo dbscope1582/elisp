9936207d14d64b466ce37fb2dc19e135b425ecc6
papa <dbscope1582@gmail.com>
2020-09-21 13:36:27 +0200

    improve the init hooks file

--- machine/scope-mob66_initHooks.el
+++ machine/scope-mob66_initHooks.el
@@ -8,4 +8,19 @@
       ))
   (add-to-list 'auto-mode-alist '("\\.asciidoc\\'" . adoc-mode))
   (add-to-list 'auto-mode-alist '("\\.cs\\'" . c-mode))
+  (unless (package-installed-p 'sr-speedbar)
+    ;; see https://www.emacswiki.org/emacs/SrSpeedbar
+    (package-install 'sr-speedbar)
+    (require 'sr-speedbar))
+  (unless (package-installed-p 'restclient)
+    (package-install 'restclient)
+    (require 'restclient))
   )
+
+;; sr-speedbar-open	Open sr-speedbar window.
+;; sr-speedbar-close	Close sr-speedbar window.
+;; sr-speedbar-toggle	Toggle sr-speedbar window.
+;; sr-speedbar-select-window	select sr-speedbar window.
+;; sr-speedbar-refresh-turn-on	Turn on refresh speedbar content.
+;; sr-speedbar-refresh-turn-off	Turn off refresh speedbar content.
+;; sr-speedbar-refresh-toggle	Toggle refresh speedbar content.
