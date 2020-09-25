ec0f5c1fab452c1b33d5a427398ad21fbfa73cf6
papa <dbscope1582@gmail.com>
2020-09-25 06:47:30 +0200

    added some style customizing
    and some more dummy lines because I try this out from emacs

--- machine/scope-mob66_initHooks.el
+++ machine/scope-mob66_initHooks.el
@@ -6,15 +6,24 @@
       (display-line-numbers-mode)
       (auto-fill-mode -42 )
       ))
+  (add-hook
+   'sql-mode-hook
+   '(lambda ()
+     (display-line-numbers-mode)))
   (add-to-list 'auto-mode-alist '("\\.asciidoc\\'" . adoc-mode))
   (add-to-list 'auto-mode-alist '("\\.cs\\'" . c-mode))
-  (unless (package-installed-p 'sr-speedbar)
-    ;; see https://www.emacswiki.org/emacs/SrSpeedbar
-    (package-install 'sr-speedbar)
-    (require 'sr-speedbar))
-  (unless (package-installed-p 'restclient)
-    (package-install 'restclient)
-    (require 'restclient))
+  ;; from https://emacs.stackexchange.com/questions/17431/how-do-i-change-portions-of-a-custom-theme
+  
+  (mapcar (lambda (x)
+	  (unless (package-installed-p x)
+	    (package-install x)
+	    (require x))
+	  )
+	  '( restclient sr-speedbar graphviz-dot-mode))
+  ;; auto revert mode (files modified on disk)
+  (global-auto-revert-mode 1)
+  ;; auto refresh dired when file changes
+  (add-hook 'dired-mode-hook 'auto-revert-mode)
   )
 
 ;; sr-speedbar-open	Open sr-speedbar window.
@@ -24,3 +33,34 @@
 ;; sr-speedbar-refresh-turn-on	Turn on refresh speedbar content.
 ;; sr-speedbar-refresh-turn-off	Turn off refresh speedbar content.
 ;; sr-speedbar-refresh-toggle	Toggle refresh speedbar content.
+
+
+;;(load-theme 'misterioso)  auch OK
+;;(load-theme 'tango-dark) modeline ist übel
+;; (list-colors-display) für namen (und preview)
+;;; section to set my individual theme. After some playing around and for now, I go for
+;;; tsdh-dark. 
+(progn
+  ;; see https://github.com/emacs-jp/replace-colorthemes/
+  ;; https://github.com/emacs-jp/replace-colorthemes/blob/master/screenshots.md#digital-ofs1
+  (let (
+	;;	( name 'digital-ofs1)
+	(name 'tsdh-dark)
+	)
+    (load-theme name t t)
+    (enable-theme name)
+    ;; allows to overwrite just a given option of the standard theme
+    ;; see also the themes defined in ~/.emacs.d/elpa/color-theme-modern-20200729.921/
+    ;; for a better idea of what is possible
+    (custom-theme-set-faces
+     name
+     ;; Blanched Almond is sligthly darker than the default. 
+     '(default ((t (:foreground "Blanched Almond"))))
+     ;;     '(font-lock-comment-face ((t (:foreground "#DFAF8F"))))   
+     )
+    )
+  ;;    (load-theme 'tsdh-dark) ;ziemlich gut00
+  ;;   (set-background-color "bisque4")
+  ;;   (set-foreground-color "black")
+  )
+
